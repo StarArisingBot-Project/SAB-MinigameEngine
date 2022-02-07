5155c5bc197a25d8806b6dcccdb55d8791e6d57d
Starciad <69594923+Starciad@users.noreply.github.com>
2022-02-07 16:09:52 -0300

    🎉 First Commit

---
+++ SABMinigameEngine/Components/Sessions/MinigameSession.cs
@@ -0,0 +1,73 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SABMinigamesEngine
+{
+    /// <summary>
+    /// Represents a minigame that is running.
+    /// </summary>
+    public sealed class MinigameSession
+    {
+        /// <summary>
+        /// The ID of the current session.
+        /// </summary>
+        public ulong SectionID { get; internal set; }
+
+        /// <summary>
+        /// The context in which the session is running.
+        /// </summary>
+        public CommandContext Context { get; private set; }
+
+        /// <summary>
+        /// The date this session was created.
+        /// </summary>
+        public DateTime CreationTimestamp { get; private set; }
+
+        /// <summary>
+        /// Current session information.
+        /// </summary>
+        public MinigameSessionInfos SessionInfos { get; private set; }
+
+        internal MinigameModule MinigameModule { get; private set; }
+        internal MinigameInstance CurrentInstance { get; private set; }
+        //======================================//
+
+        public delegate void SessionDisconnected();
+
+        /// <summary>
+        /// Invoked if the session is disconnected.
+        /// </summary>
+        public event SessionDisconnected OnSessionDisconnected;
+
+        //======================================//
+
+        internal MinigameSession(CommandContext context, MinigameInstance currentInstance, MinigameModule minigameModule, MinigameSessionBuilder sessionBuilder = null)
+        {
+            Context = context;
+            CurrentInstance = currentInstance;
+            MinigameModule = minigameModule;
+            MinigameModule.Session = this;
+
+            CreationTimestamp = DateTime.Now;
+
+            if(sessionBuilder == null)
+            {
+                SessionInfos = new MinigameSessionInfos();
+            }
+            else
+            {
+                SessionInfos = sessionBuilder.ToSessionInfos();
+            }
+        }
+        internal async Task DisconnectAsync()
+        {
+            MinigameModule.StartCancelProcess();
+            await CurrentInstance.RemoveSessionAsync(SectionID);
+        }
+    }
+}
---
+++ SABMinigameEngine/Managers/MinigameInstanceClient.cs
@@ -0,0 +1,87 @@
+﻿using DSharpPlus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace SABMinigamesEngine
+{
+    /// <summary>
+    /// Manager responsible for controlling the flow of minigames.
+    /// </summary>
+    public static class MinigameInstanceClient
+    {
+        /// <summary>
+        /// Represents the instance of each loaded minigame module.
+        /// </summary>
+        public static MinigameInstance[] MinigameInstances { get; private set; }
+
+        private static Assembly CurrentAssembly { get; set; }
+        private static DiscordClient Client { get; set; }
+
+        //======================================================//
+
+        /// <summary>
+        /// Launch the minigame client.
+        /// </summary>
+        /// <param name="client">The current Discord Client connected.</param>
+        public static async Task StartAsync(DiscordClient client)
+        {
+            Client = client;
+            CurrentAssembly = typeof(MinigameInstanceClient).Assembly;
+
+            await GetMinigamesInstances();
+            await StartMinigamesModules();
+
+            //==========================//
+            MinigameConsoleDebug();
+        }
+
+        private static void MinigameConsoleDebug()
+        {
+            string minigames = "";
+            foreach (MinigameInstance minigame in MinigameInstances)
+            {
+                minigames += $"> {minigame.MinigameModule.Name} \n";
+            }
+
+            Console.WriteLine($"\n=================\n" +
+
+                              $"INSTANCES LOADED: \n\n" +
+
+                              $"{minigames}" +
+
+                              $"\n=================\n");
+        }
+        private static Task GetMinigamesInstances()
+        {
+            List<MinigameInstance> instances = new();
+            foreach (Type minigame in CurrentAssembly.GetTypes().Where(x => x.IsSubclassOf(typeof(MinigameModule))))
+            {
+                if (minigame.Name.EndsWith("Minigame"))
+                {
+                    instances.Add(new MinigameInstance(minigame));
+                }
+            }
+
+            MinigameInstances = instances.ToArray();
+            return Task.CompletedTask;
+        }
+        private static Task StartMinigamesModules()
+        {
+            return Task.CompletedTask;
+        }
+
+        //======================================================//
+        /// <summary>
+        /// Get the active instance of a minigame.
+        /// </summary>
+        /// <param name="minigameModuleType">The name of the desired instance.</param>
+        /// <returns>The required instance.</returns>
+        public static async Task<MinigameInstance> GetInstanceAsync(Type minigameModuleType)
+        {
+            return await Task.FromResult(MinigameInstances.Where(x => x.MinigameModule == minigameModuleType).FirstOrDefault());
+        }
+    }
+}
